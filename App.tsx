b3f7a2918678840571a82fcd12320406f1d2a6fb
Matheus Botezelli <mbtz27@outlook.com>
2024-08-25 23:41:54 -0300

    add char base64 conversion

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import {Button, StyleSheet, View} from 'react-native';
-import {ScanDevices} from "./src/modules/BluetoothModule";
+import {
+    GetDeviceFactoryIdentification,
+    GetDeviceFirmwareInformation,
+    ScanDevices
+} from "./src/modules/BluetoothModule";
 
 export default function App() {
     return (
         <View style={styles.container}>
-            <Button title="Scan Devices" onPress={ScanDevices}></Button>
+            <Button title="Scan and Connect" onPress={ScanDevices}></Button>
+            <Button title="Device Factory Identification" onPress={GetDeviceFactoryIdentification}></Button>
+            <Button title="Device Firmware Identification" onPress={GetDeviceFirmwareInformation}></Button>
         </View>
     );
 }
--- src/modules/BluetoothModule.ts
+++ src/modules/BluetoothModule.ts
@@ -1,7 +1,23 @@
 import {BleManager, Device} from 'react-native-ble-plx';
+import {Buffer} from 'buffer'
 
-const manager = new BleManager();
+let manager: BleManager = new BleManager();
 let devices: Device[] = [];
+let connectedDevice: Device;
+
+const DeviceUUID: string = "5B732855-6C98-BE5C-53DA-42DEF038D059";
+
+const DeviceInformationService: string = "812B5541-3E76-44AC-A119-27EADFB48B4F";
+const DeviceFactoryIdentificationCharacteristic: string = "FE8DF3D7-807A-4514-993F-146ED5B76E32";
+const DeviceFirmwareInformationCharacteristic: string = "3E240DAD-B045-434F-A4E1-F022BBC4F767";
+
+const DeviceConfigurationService: string = "FE1637F9-190C-411A-B1BD-ACFDC391C9F3";
+const DeviceSettingsCharacteristic: string = "2EDF6646-D783-4FDC-9845-FA0D9E3D8807";
+const CarSettingsCharacteristic: string = "106FA464-71 FB-41 FF-81B3-5A1148E2B2E9";
+const PlayerSettingsCharacteristic: string = "60E869FA-84E5-4EE7-A58A-EB3640D13367";
+
+const PhoneRemoteControlService: string = "B9653801-9A6B-4C53-A3E9-2EFE67C2C931";
+const PhoneCommandCharacteristic: string = "544B8542-0C08-4009-B554-82A3708F3D8C";
 
 export function ScanDevices(): void {
     devices = [];
@@ -12,18 +28,37 @@ export function ScanDevices(): void {
         }
         if (device != null && !devices.some(x => x.id === device.id)) {
             devices.push(device);
-            // console.log(`ID: ${device?.id} Name: ${device?.name}`);
+            console.log(`ID: ${device?.id} Name: ${device?.name}`);
         }
     }).then(async () => {
         await sleep(4000);
         await manager.stopDeviceScan();
         console.log("Scan finished");
-        let device = await manager.connectToDevice("5FF05834-B58F-4974-69F2-83A766288DAA");
-        await device.discoverAllServicesAndCharacteristics();
-        await device.readCharacteristicForService("812B5541-3E76-44AC-A119-27EADFB48B4F", "FE8DF3D7-807A-4514-993F-146ED5B76E32");
+        connectedDevice = await manager.connectToDevice(DeviceUUID);
+        console.log(`Connected to device: ${DeviceUUID}`);
+        await connectedDevice.discoverAllServicesAndCharacteristics();
     });
+}
+
+export async function GetDeviceFactoryIdentification(): Promise<void> {
+    let characteristic = await connectedDevice.readCharacteristicForService(DeviceInformationService, DeviceFactoryIdentificationCharacteristic);
+    console.log(`Raw value: ${characteristic.value}`);
+    let result = base64ToBigEndianBytes(characteristic.value!);
+    console.log(`Device Factory Identification: ${result}`);
+}
+
+export async function GetDeviceFirmwareInformation(): Promise<void> {
+    let result = await connectedDevice.readCharacteristicForService(DeviceInformationService, DeviceFirmwareInformationCharacteristic);
+    console.log(`Device Firmware Information: ${result.value}`);
 }
 
 function sleep(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function base64ToBigEndianBytes(base64: string): Uint8Array {
+    const buffer = Buffer.from(base64, 'base64');
+    console.log(buffer);
+    const byteArray = new Uint8Array(buffer);
+    return byteArray.reverse();
 }
